49aa106b46201a610bca4021cdf58e8e72c5870f
Mason-GR <mase210@gmail.com>
2022-02-15 22:40:22 -0500

    frustration intensifies

---
+++ app.js
@@ -0,0 +1,71 @@
+// Book Class: Represents a Book
+
+class Book {
+  constructor(title, author, isbn) {
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
+  }
+}
+
+// UI Class: Handles UI (User Interface) Tasks
+
+class UI {
+  static displayBooks() {
+    const storedBooks = [
+      {
+        title: "The Name of The Wind",
+        author: "Patrick Rothfuss",
+        isbn: "58412",
+      },
+      {
+        title: "The Eye of the World",
+        author: "Robert Jordan",
+        isbn: "91668",
+      },
+    ];
+
+    const books = storedBooks;
+
+    books.forEach((book) => UI.addBookToList(book));
+  }
+
+  static addBookToList(book) {
+    const list = document.querySelector("#book-list");
+
+    const row = document.createElement("tr");
+
+    row.innerHTML = `
+    <td>${book.title}</td>
+    <td>${book.author}</td>
+    <td>${book.isbn}</td>
+    <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>`;
+
+    list.appendChild(row);
+  }
+}
+
+// Store Class: Handles Storage of the Books
+
+// Event: Display Books
+
+document.addEventListener("DOMContentLoaded", UI.displayBooks);
+
+// Event: Add a Book
+
+document.querySelector("#book-form").addEventListener("submit", (e) => {
+  // Prevent submit
+  e.preventDefault();
+
+  // Get form values
+  const title = document.querySelector("#title").value;
+  const author = document.querySelector("#author").value;
+  const isbn = document.querySelector("#isbn").value;
+
+  // Instantiate the book
+  const book = new Book(title, author, isbn);
+
+  console.log(book);
+});
+
+// Event: Remove a Book
